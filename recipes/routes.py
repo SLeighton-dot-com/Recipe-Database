d3232fc9ba4f7753d1430938ae6fae712d78f02e
SLeighton-dot-com <>
2023-11-14 13:31:18 +0000

    routes.py:Added more imports from flask, added app.route for add_recipe

--- recipes/routes.py
+++ recipes/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from recipes import app, db
 from recipes.models import Filter, Recipe
 
@@ -10,4 +10,25 @@ def home():
 
 @app.route("/filters")
 def filters():
-    return render_template("filters.html")
+    filters = list(Filter.query.order_by(Filter.filter_name).all())
+    return render_template("filters.html", filters=filters)
+
+
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    filters = list(Filter.query.order_by(Filter.filter_name).all())
+    if request.method == "POST":
+        category = Category(
+            recipe_name=request.form.get("recipe_name"),
+            recipe_description=request.form.get("recipe_description"),
+            main_ingredient=request.form.get("main_ingredient"),
+            cooking_method=request.form.get("cooking_method"),
+            prep_time=request.form.get("prep_time"),
+            recipe_owner=request.form.get("recipe_owner")
+        )
+        db.session.add(recipe)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_recipe.html")
+
+
