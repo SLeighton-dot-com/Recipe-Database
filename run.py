4d83a706ef87ad5563675021af6fa09632ae48a8
SLeighton-dot-com <>
2023-11-17 18:39:22 +0000

    run.py:Added code for Heroku

--- run.py
+++ run.py
@@ -3,6 +3,11 @@ from recipes import app
 
 
 if __name__ == "__main__":
+
+    def create_tables(self):
+        with app.app_context():
+            db.create_all()
+    create_tables(db)
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
