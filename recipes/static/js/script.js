56a88f5114d2fc591dc9d4a8b313d678685183fe
SLeighton-dot-com <>
2023-11-17 17:10:36 +0000

    script.js:Added script to initialize collapsibles on recipes.html

--- recipes/static/js/script.js
+++ recipes/static/js/script.js
@@ -2,3 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav, {});
 });
+
+let collapsibles = document.querySelectorAll(".collapsible");
+M.Collapsible.init(collapsibles);
