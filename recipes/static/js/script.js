433cc36bf743551d44cc93e67c38f184e93a9965
SLeighton-dot-com <>
2023-12-22 14:06:17 +0000

    script.js:Added initialization for selects

--- recipes/static/js/script.js
+++ recipes/static/js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav, {});
+
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
 
 let collapsibles = document.querySelectorAll(".collapsible");
