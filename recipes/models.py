f0d7107bfc54f457312d47585e15277cd62cfd8d
SLeighton-dot-com <>
2023-12-22 16:45:32 +0000

    models.py:Added users database

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,5 @@
 from recipes import db
-from werkzeug.security import generate_password_hash, check_password_hash
-
+from werkzeug.security import generate_password_hash
 
 class Filter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +9,18 @@ class Filter(db.Model):
 
     def __repr__(self):
         return self.filter_name
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 
 class Recipe(db.Model):
