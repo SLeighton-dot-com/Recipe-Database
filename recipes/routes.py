260ad0526008172e7eaf8489a8975fb55d2f5ac2
SLeighton-dot-com <>
2023-12-22 16:48:57 +0000

    routes.py:Import werkzeug password hash and added register function

--- recipes/routes.py
+++ recipes/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from recipes import app, db
 from recipes.models import Filter, Recipe
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route("/")
@@ -15,28 +16,48 @@ def filters():
     return render_template("filters.html", filters=filters)
 
 
-@app.route("/add_recipe", methods=["GET", "POST"])
-def add_recipe():
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username").lower()
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+
+        if password != password2:
+            flash("Passwords do not match.")
+            return redirect(url_for("register"))
+            
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        new_user = User(username=username)
+        new_user.set_password(request.form.get("password"))
+        db.session.add(new_user)
+        db.session.commit()
+
+        session["user"] = username
+        flash("Registration Successful!")
+    return render_template("register.html")
+
+
+@app.route("/edit_recipe/<int:recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
     filters = list(Filter.query.order_by(Filter.filter_name).all())
     if request.method == "POST":
-        prep_time_input = request.form.get("prep_time")
-        try:
-            prep_time_value = int(prep_time_input)
-        except ValueError:
-            prep_time_value = 0
-        recipe = Recipe(
-            recipe_name=request.form.get("recipe_name"),
-            recipe_description=request.form.get("recipe_description"),
-            main_ingredient=request.form.get("main_ingredient"),
-            cooking_method=request.form.get("cooking_method"),
-            prep_time=prep_time_value,
-            recipe_owner=request.form.get("recipe_owner"),
-            category_id=request.form.get("category_id")
-        )
-        db.session.add(recipe)
+        recipe.recipe_name = request.form.get("recipe_name"),
+        recipe.recipe_description = request.form.get("recipe_description"),
+        recipe.main_ingredient = request.form.get("main_ingredient"),
+        recipe.cooking_method = request.form.get("cooking_method"),
+        recipe.prep_time = request.form.get("prep_time"),
+        recipe.recipe_owner = request.form.get("recipe_owner"),
+        recipe.category_id = request.form.get("category_id")
         db.session.commit()
-        return redirect(url_for("home"))
-    return render_template("add_recipe.html", filters=filters)
+    return render_template("edit_recipe.html", recipe=recipe, filters=filters)
+
 
 
 @app.route("/edit_recipe/<int:recipe_id>", methods=["GET", "POST"])
