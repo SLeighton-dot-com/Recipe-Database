184a303b7b2968a28124bb289ad88549f15f8ff5
SLeighton-dot-com <>
2024-01-16 10:52:19 +0000

    routes.py:Edited edit_recipe function with correct varible name

--- recipes/routes.py
+++ recipes/routes.py
@@ -111,7 +111,9 @@ def edit_recipe(recipe_id):
         recipe.main_ingredient = request.form.get("main_ingredient")
         recipe.cooking_method = request.form.get("cooking_method")
         recipe.prep_time = prep_time_value
-        recipe.category_id = request.form.get("category_id")
+        recipe.category_id = request.form.get("filter_id")
+        if filter_id_input:
+            recipe.category_id = filter_id_input
         db.session.commit()
         flash("Recipe updated successfully!")
     return render_template("edit_recipe.html", recipe=recipe, filters=filters)
