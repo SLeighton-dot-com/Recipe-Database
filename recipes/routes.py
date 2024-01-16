cbd8d9857a38aac726d36220cf55800210645fee
SLeighton-dot-com <>
2024-01-16 11:09:43 +0000

    routes.py:Added return redirect after recipe is updated

--- recipes/routes.py
+++ recipes/routes.py
@@ -116,6 +116,7 @@ def edit_recipe(recipe_id):
             recipe.category_id = filter_id_input
         db.session.commit()
         flash("Recipe updated successfully!")
+        return redirect(url_for("home"))
     return render_template("edit_recipe.html", recipe=recipe, filters=filters)
 
 
