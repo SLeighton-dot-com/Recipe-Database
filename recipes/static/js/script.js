1b38334e9f7fb2621f7e2c3d3f9605c901f54bd2
SLeighton-dot-com <>
2024-01-16 11:19:43 +0000

    script.js:Added comments to code

--- recipes/static/js/script.js
+++ recipes/static/js/script.js
@@ -1,10 +1,17 @@
+// Wait for the webpage to load completely before running the code inside.
 document.addEventListener('DOMContentLoaded', function () {
+    // Find elements on the page that are used for the side menu (like a navigation drawer).
     let sidenav = document.querySelectorAll('.sidenav');
+    // Set up the side menu so it works (opens and closes) correctly.
     M.Sidenav.init(sidenav, {});
 
+    // Find dropdown menu elements on the page.
     let selects = document.querySelectorAll("select");
+    // Set up the dropdown menus to look nice and work properly.
     M.FormSelect.init(selects);
 });
 
+// Find elements on the page that can be opened and closed like an accordion.
 let collapsibles = document.querySelectorAll(".collapsible");
+// Set up these elements so they can be opened and closed.
 M.Collapsible.init(collapsibles);
